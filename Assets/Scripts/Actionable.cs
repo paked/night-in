b919dbee84ef27cd48d4acfae96cecc038200ba3
Harrison Shoebridge <harrison@theshoebridges.com>
2017-01-21 09:59:40 -0800

    Add GoogleVR and Actionable/Trigger system

---
+++ Assets/Scripts/Actionable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actionable : MonoBehaviour {
+	public void DoAction() {
+		Debug.Log (transform.name + " is doing an action!");
+
+		transform.position = new Vector3 (
+			transform.position.x,
+			transform.position.y + 1f * Time.deltaTime,
+			transform.position.z
+		);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ActionableTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionableTrigger : MonoBehaviour {
+	public Camera cam;
+
+	void Start() {
+		cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var ray = new Ray(cam.transform.position, cam.transform.forward);
+		var hit = new RaycastHit();
+
+		if(!Physics.Raycast (ray, out hit)) {
+			Debug.Log ("Did not hit anything");
+			return;
+		}
+
+		var actionable = hit.transform.GetComponent<Actionable> ();
+		if (actionable == null) {
+			Debug.Log ("Object is not an Actionable");
+			return;
+		}
+
+		actionable.DoAction ();
+//
+//		Debug.Log (hit.transform.name + " was hit!");
+//		var newPos = new Vector3 (
+//			hit.transform.position.x,
+//			hit.transform.position.y + 2f * Time.deltaTime,
+//			hit.transform.position.z
+//		);
+//
+//		hit.transform.position = newPos;
+	}
+}
