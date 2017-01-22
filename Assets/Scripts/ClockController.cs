02d3905e8c4e85e6c15a4da0f6c008bbf59a8af8
Jessica Kwok <jk3405@nyu.edu>
2017-01-21 22:49:43 -0800

    Finish clock and really crappy imitation time

--- Assets/Scripts/ClockController.cs
+++ Assets/Scripts/ClockController.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class ClockController : MonoBehaviour {
 
+	private Transform hourH;
+	private Transform minH;
+	private Transform secH;
+
+	private float hourRate; // min per hour
+	private float minRate; // sec per min
+	private float secRate; // how fast is time moving
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+		// start at 8PM (implied PM)
+		hourH = transform.FindChild("hourHand");
+		minH = transform.FindChild("minuteHand");
+		secH = transform.FindChild("secondHand");
+
+		hourRate = 6;
+		minRate = 10;
+		secRate = 1;
 	}
-	
+
+
+
+	void moveHour() {
+		hourH.rotation *= Quaternion.AngleAxis (secRate/(minRate*hourRate), Vector3.down);
+	}
+
+	void moveMin() {
+		minH.rotation *= Quaternion.AngleAxis (secRate/minRate, Vector3.down);
+	}
+
+	void moveSec() {
+		secH.rotation *= Quaternion.AngleAxis (secRate, Vector3.down);
+	}
 	// Update is called once per frame
 	void Update () {
-		
+		moveHour ();
+		moveMin ();
+		moveSec ();
+		//Time.deltaTime);
 	}
 }
